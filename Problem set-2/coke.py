7944454497a5951ad4a34fbba69356126301bc1d
Riya Patel <riya.patell1204@gmail.com>
2022-12-24 14:55:07 +0530

    loops

---
+++ Problem set-2/coke.py
@@ -0,0 +1,11 @@
+def main():
+    amount_due = 50
+    print("Amount Due: ", amount_due)
+    insert_coin = input("Insert Coin: ")
+    if insert_coin == "5" or "10" or "25":
+        print(int(amount_due) - int(insert_coin))
+    elif():
+        print(amount_due)
+
+
+main()
